135860f61d223718c742a02e87dc7e6f2f67160a
Janco de Beer <debeerjanco20@gmail.com>
2024-07-19 19:34:43 +0200

    initial commit

---
+++ lib/appwrite.config.ts
@@ -0,0 +1,30 @@
+import * as sdk from "node-appwrite";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const {
+  PROJECT_ID,
+  API_KEY,
+  DATABASE_ID,
+  PATIENT_COLLECTION_ID,
+  DOCTOR_COLLECTION_ID,
+  APPOINTMENT_COLLECTION_ID,
+  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+} = process.env;
+
+if (!PROJECT_ID || !API_KEY || !ENDPOINT) {
+  throw new Error(
+    "Missing necessary environment variables for Appwrite configuration."
+  );
+}
+
+const client = new sdk.Client();
+
+client.setEndpoint(ENDPOINT).setProject(PROJECT_ID).setKey(API_KEY);
+
+export const databases = new sdk.Databases(client);
+export const storage = new sdk.Storage(client);
+export const messaging = new sdk.Messaging(client);
+export const users = new sdk.Users(client);
